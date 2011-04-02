882ab74467a8a423f4f495ee845d53162a6d20a4
admin <eatmmtg@gmail.com>
2011-04-01 23:04:49 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cramcards::Application.config.secret_token = 'e4b87f12c805f5949e0557012606004976b7102551c40db80bf019f1bc117a9ba0a4d8a2c6132cb5a37d2e379597d888723697808b542c9d3a27c90d2f36f8a5'
